5976d38acfe64b3b596daf7bf3811626ab56c512
PL <philip.lundin@iths.se>
2021-10-28 16:53:59 +0200

    new design

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,9 @@
    <nav>
      <router-link style="padding-right: 1%" to="/"><img src="../src/assets/logo.png"></router-link>
      <router-link class="a" to="/">Home</router-link>
-     <router-link class="a" to="/mypage">My Page</router-link>
-     <router-link class="a" to="/login">Login</router-link>
-     <router-link class="a" to="/signup">Signup</router-link>
-     <router-link class="a" to="/contact">Contact</router-link>
-     <router-link class="a" to="/about">About</router-link>
+     <router-link class="a" to="/mypage">My page</router-link>
+     <router-link class="a" to="/signup">Sign up</router-link>
+     <router-link class="b" to="/login">Login</router-link>
    </nav>
    </header>
     <router-view/>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,34 @@
 <template>
 
       <div class="home">
-        <p>TAKE A QUIZ</p>
+        <p id="padding">TAKE A QUIZ</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda aut consectetur<br> deserunt, esse excepturi exercitationem, expedita, facere magni molestiae nam<br> non perspiciatis quidem sequi soluta tempora ut velit voluptatibus.</p>
         <section class="grid-container">
       <main>
         <article>
+          <p>Addition </p>
           <router-link to="/"><img src="../assets/subtraction.png"/></router-link>
         </article>
         <article>
+          <p>Subtraction</p>
           <router-link to="/"><img src="../assets/subtraction.png"/></router-link>
         </article>
         <article>
+          <p>Multiplication</p>
           <router-link to="/"><img src="../assets/subtraction.png"/></router-link>
         </article>
         <article>
+          <p>Division</p>
           <router-link to="/"><img src="../assets/subtraction.png"/></router-link>
         </article>
       </main>
-
-      </section>
+        </section>
         <footer>
-          <h2>Mathlingo</h2>
-          &copy; 2021 All rights reserved
-          <a style="float:right" href="tel:070-000-0000">070-000-0000</a>
+          <p>Mathlingo &copy; 2021 All rights reserved.</p>
+          <p><router-link style="margin: 5px" to="/contact">Contact us</router-link>| <router-link to="/about">About</router-link></p>
         </footer>
+
+
       </div>
 
 
