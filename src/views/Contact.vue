11143df7e5a330b3f8c1f916a3caf11098f41871
Gustav Jakobsson <gustavJak94@gmail.com>
2021-11-02 10:54:05 +0100

    contact form sends email

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,19 +3,20 @@
     <section class="grid-container">
       <main id="subPages">
 
-        <form id="contact"  method="post">
+        <form id="contact"  method="post" >
 
           <h1>Contact the team</h1>
-          <input type="text" placeholder="Enter Name" name="contactname" required id="contactName">
+          <form v-on:submit.prevent="sendEmail">
+          <input type="text" placeholder="Enter Name" name="contactname" required id="contactName" v-model="name">
 
-          <input type="text" placeholder="Enter Email" name="email" required id="contactEmail">
+          <input type="text" placeholder="Enter Email" name="email" required id="contactEmail" v-model="email">
 
-          <input type="text" placeholder="Topic" name="topic" required id="contactTopic">
+          <input type="text" placeholder="Topic" name="topic" required id="contactTopic" v-model="topic">
 
-          <textarea name="message" rows="5" placeholder="Message.." required id="contactMessage"></textarea>
+          <textarea name="message" rows="5" placeholder="Message.." required id="contactMessage" v-model="message"></textarea>
 
           <input type="submit" class ="btn" id="btn-contact" value="Send">
-
+          </form>
         </form>
       </main>
     </section>
@@ -27,7 +28,38 @@
 </template>
 
 <script>
+
+import emailjs from 'emailjs-com'
+
+
 export default {
-  name: "Contact"
+  name: "Contact",
+  data() {
+  return {
+  name: '',
+  email: '',
+    topic: '',
+  message: ''
+}
+},
+  methods: {
+  sendEmail(e) {
+  try {
+  emailjs.sendForm("SERVICE ID","TEMPLATE ID", e.target,
+  'USER ID', {
+  name: this.name,
+  email: this.email,
+  message: this.message
+})
+
+} catch(error) {
+  console.log({error})
+}
+  this.name = ''
+  this.email = ''
+  this.message = ''
+},
+}
 }
 </script>
+
