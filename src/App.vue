26818cb0ee607212d891dbe6b8a50c8e4aa1ab3f
Gustav Jakobsson <gustavJak94@gmail.com>
2021-11-11 09:48:23 +0100

    logout

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
      <!-- logo & text ska göras till en gemensam img  -->
    <div id="logo"> <router-link to="/"><img src="../src/assets/logo.png"></router-link>Mathlingo</div>
-
    <nav>
      <router-link class="a" to="/">Home</router-link>
      <router-link class="a" to="/mypage" v-if="isLoggedIn">My page</router-link>
@@ -12,10 +11,6 @@
      <router-link class="a" to="/about">About</router-link>
      <router-link class="a" to="/signup" v-if="isLoggedIn===null">Sign up</router-link>
      <router-link class="a" to="/login" v-if="isLoggedIn===null">Login</router-link>
-     <router-link class="a" to="/login" v-if="isLoggedIn" v-on:click="logout">Logout</router-link>
-
-
-
    </nav>
    </header>
     <router-view/>
@@ -33,13 +28,8 @@ export default {
     }
   },
   methods:{
-    logout(){
-      window.localStorage.clear();
-      this.$router.push('/login');
+
     }
-  }
-
-
 
 }
 
--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -3,8 +3,8 @@
 
     <section class="grid-container">
       <main id="subPages">
-        <button v-on:click="logout">logout</button>
-        <h1>My page</h1>
+     <h1><span class="logout-btn" v-on:click="logout"><a></a></span></h1>
+
 
       </main>
     </section>
@@ -17,6 +17,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "MyPage",
   data(){
@@ -26,8 +28,13 @@ export default {
   },
   methods:{
     logout(){
+     let token = window.localStorage.getItem('accessToken')
+      axios.post('http://localhost:4000/auth/logout', token);
       window.localStorage.clear();
-      this.$router.push('/login');
+      this.$router.push('/contact');
+      location.reload();
+
+
 
     }
   }
