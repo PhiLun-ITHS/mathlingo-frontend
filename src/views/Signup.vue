0a534e693ae15e37421e37a15c531188a5844f37
milan <milanarif@gmail.com>
2021-11-18 01:12:43 +0100

    signup fixed

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -8,13 +8,13 @@
           <h1>Sign up for Mathlingo</h1>
         <!--<p id="answer"></p>-->
           <form v-on:submit.prevent="submitForm">
-            <input type="text" placeholder="Enter Name" name="uname" required id="userName" v-model="name">
+            <input type="text" placeholder="Enter Name" name="uname" id="userName" v-model="name">
 
-            <input type="text" placeholder="Enter Email" name="email" required id="email" v-model="email">
+            <input type="text" placeholder="Enter Email" name="email" id="email" v-model="email">
 
-            <input type="password" placeholder="Enter Password" name="psw" required id="password" v-model="password">
+            <input type="password" placeholder="Enter Password" name="psw" id="password" v-model="password">
 
-            <input type="password" placeholder="Confirm Password" name="psw" required id="rePassword" v-model="rePassword">
+            <input type="password" placeholder="Confirm Password" name="psw" id="rePassword" v-model="rePassword">
 
             <input type="submit" class ="btn" id="btn-signup" value="Register">
 
@@ -64,9 +64,10 @@ export default {
       }
       else this.validName = false;
 
-      if (this.matchingPass == this.validName == this.validEmail == true) {
+      if (this.matchingPass == this.validName && this.matchingPass == true && this.matchingPass == this.validEmail) {
         this.validForm = true;
-      } else {
+      }
+      else {
         this.validForm = false;
       }
 
@@ -82,19 +83,65 @@ export default {
             this.emailAvailable = response.data;
             if (this.emailAvailable) {
               let user = {name: this.name, email: this.email, password: this.password};
-              console.log(user);
               axios.post('http://localhost:4000/auth/signup', user)
               .then(() => {
                 swal.fire({
-                  title: "Account Created Successfully!",
-                  confirmButtonText: 'Proceed',
-                  confirmButtonColor: 'blue'
+                  title: 'Account Created Successfully!',
+                  text: 'Login to continue',
+                  icon: 'success',
+                  confirmButtonText: 'Login',
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    this.$router.push('/login');
+                  }
+                  else this.resetForm();
                 })
               })
             }
           })
       }
+      else {
+        if (!this.validName) {
+          swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Name is too short. Has to be at least 3 characters.'
+          })
+          .then(() => {
+            this.password = '';
+            this.rePassword = '';
+          })
+        }
+        else if (!this.validEmail) {
+          swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Email is not valid.'
+          })
+          .then(() => {
+            this.password = '';
+            this.rePassword = '';
+          })
+        }
+        else if (!this.matchingPass) {
+          swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'The passwords do not seem to match. Try again!'
+          })
+          .then(() => {
+            this.password = '';
+            this.rePassword = '';
+          })
+        }
+      }
     },
+    resetForm(){
+      this.name = '';
+      this.email = '';
+      this.password = '';
+      this.rePassword = '';
+    }
   },
   computed: {
     changeFormData() {
